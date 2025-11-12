45d7e08473aba645f8dadd026351597e820193ba
nicolvisser <vissernicol@gmail.com>
2025-11-12 12:55:52 +0200

    eval-script cleanup and format

--- eval-boundaries.py
+++ eval-boundaries.py
@@ -1,29 +1,24 @@
 from pathlib import Path
-from tqdm.autonotebook import tqdm
-import torchaudio
-import tgt
 
-from zerosyl.utils.boundaries import *
+import tgt
+import torchaudio
+from tqdm.autonotebook import tqdm
+
 from zerosyl.model import ZeroSylBase
+from zerosyl.utils.boundaries import *
 
 TOLERANCE = 0.05  # 50 ms
 
 checkpoint_path = Path("checkpoints/WavLM-Large.pt")
-waveform_dir = Path.cwd() / "data/waveforms/LibriSpeech/dev-clean"
-alignment_dir = Path.cwd() / "data/alignments/LibriSpeech/dev-clean"
-
-# TODO Delete below:
-checkpoint_path = Path("/media/hdd/models/WavLM-Large.pt")
-waveform_dir = Path.cwd() / "data"
-alignment_dir = Path.cwd() / "data"
-# ~~~~~~~~~~~~~~~~~
+waveform_dir = Path.cwd() / "data/waveforms/LibriSpeech"
+alignment_dir = Path.cwd() / "data/alignments/LibriSpeech"
 
 assert checkpoint_path.exists()
 assert waveform_dir.exists()
 assert alignment_dir.exists()
 
-waveform_paths = {p.stem: p for p in waveform_dir.rglob("*.flac")}
-alignment_paths = {p.stem: p for p in alignment_dir.rglob("*.TextGrid")}
+waveform_paths = {p.stem: p for p in waveform_dir.glob("dev*/**/*.flac")}
+alignment_paths = {p.stem: p for p in alignment_dir.glob("dev*/**/*.TextGrid")}
 
 common_stems = sorted(waveform_paths.keys() & alignment_paths.keys())
 waveform_paths = [waveform_paths[s] for s in common_stems]
@@ -33,28 +28,28 @@ assert len(waveform_paths) > 0
 model = ZeroSylBase.from_pretrained_checkpoint(checkpoint_path).cuda()
 
 segs, refs = [], []
-for waveform_path, alignment_path in tqdm(zip(waveform_paths, alignment_paths)):
+for waveform_path, alignment_path in zip(tqdm(waveform_paths), alignment_paths):
     waveform, sr = torchaudio.load(str(waveform_path))
     tg = tgt.read_textgrid(alignment_path, include_empty_intervals=True)
 
-    refs.append([float(interval.end_time) for interval in tg.get_tier_by_name("syllables")])
+    refs.append(
+        [float(interval.end_time) for interval in tg.get_tier_by_name("syllables")]
+    )
     segs.append(model.boundaries(waveform.cuda()))
 
 # -------------- Calculate boundary evaluation metrics --------------
 
-precision, recall, f1 = eval_boundaries(
-    segs,
-    refs,
-    tolerance=TOLERANCE
-)
+precision, recall, f1 = eval_boundaries(segs, refs, tolerance=TOLERANCE)
 
 rvalue = get_rvalue(precision, recall)
 
 token_precision, token_recall, token_f1 = eval_token_boundaries(
-    segs,
-    refs,
-    tolerance=TOLERANCE
+    segs, refs, tolerance=TOLERANCE
 )
 
-print(f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}, R-value: {rvalue:.4f}")
-print(f"Token Precision: {token_precision:.4f}, Token Recall: {token_recall:.4f}, Token F1: {token_f1:.4f}")
+print(
+    f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}, R-value: {rvalue:.4f}"
+)
+print(
+    f"Token Precision: {token_precision:.4f}, Token Recall: {token_recall:.4f}, Token F1: {token_f1:.4f}"
+)
--- zerosyl/utils/boundaries.py
+++ zerosyl/utils/boundaries.py
@@ -1,41 +1,38 @@
-from typing import List, Tuple
-import numpy as np
 import itertools
+from typing import List, Tuple
 
-def get_p_r_f1(
-    n_seg: int, 
-    n_ref: int, 
-    n_hit: int
-) -> Tuple[float, float, float]:
-    
+import numpy as np
+
+
+def get_p_r_f1(n_seg: int, n_ref: int, n_hit: int) -> Tuple[float, float, float]:
+
     if n_seg == n_ref == 0:
         return 0, 0, -np.inf
     elif n_hit == 0:
         return 0, 0, 0
-    
+
     if n_seg != 0:
-        precision = float(n_hit/n_seg)
+        precision = float(n_hit / n_seg)
     else:
         precision = np.inf
-    
+
     if n_ref != 0:
-        recall = float(n_hit/n_ref)
+        recall = float(n_hit / n_ref)
     else:
         recall = np.inf
-    
+
     if precision + recall != 0:
-        f1_score = 2*precision*recall/(precision+recall)
+        f1_score = 2 * precision * recall / (precision + recall)
     else:
         f1_score = -np.inf
-    
+
     return precision, recall, f1_score
 
+
 def eval_boundaries(
-        seg: List[float], 
-        ref: List[float], 
-        tolerance: float = 0.05
+    seg: List[float], ref: List[float], tolerance: float = 0.05
 ) -> Tuple[float, float, float]:
-    
+
     n_seg, n_ref, n_hit = 0, 0, 0
     assert len(seg) == len(ref)
 
@@ -43,10 +40,14 @@ def eval_boundaries(
         prediction = seg[i_utterance][1:]
         ground_truth = ref[i_utterance]
 
-        if len(prediction) > 0 and len(ground_truth) > 0 and abs(prediction[-1] - ground_truth[-1]) <= tolerance:
+        if (
+            len(prediction) > 0
+            and len(ground_truth) > 0
+            and abs(prediction[-1] - ground_truth[-1]) <= tolerance
+        ):
             prediction = prediction[:-1]
             if len(ground_truth) > 0:
-                ground_truth = ground_truth[:-1] 
+                ground_truth = ground_truth[:-1]
 
         n_seg += len(prediction)
         n_ref += len(ground_truth)
@@ -60,64 +61,66 @@ def eval_boundaries(
                     n_hit += 1
                     prediction.pop(i)
                     break
-        
+
     return get_p_r_f1(n_seg, n_ref, n_hit)
-    
-def get_os(
-    precision: float, 
-    recall: float
-) -> float:
+
+
+def get_os(precision: float, recall: float) -> float:
 
     if precision == 0:
         return -np.inf
     else:
-        return recall/precision - 1
-    
-def get_rvalue(
-    precision: float, 
-    recall: float
-) -> float:
+        return recall / precision - 1
+
+
+def get_rvalue(precision: float, recall: float) -> float:
 
     os = get_os(precision, recall)
-    r1 = np.sqrt((1 - recall)**2 + os**2)
-    r2 = (-os + recall - 1)/np.sqrt(2)
+    r1 = np.sqrt((1 - recall) ** 2 + os**2)
+    r2 = (-os + recall - 1) / np.sqrt(2)
 
-    return 1 - (np.abs(r1) + np.abs(r2))/2
+    return 1 - (np.abs(r1) + np.abs(r2)) / 2
+
 
 def eval_token_boundaries(
-    seg: List[float], 
-    ref: List[float], 
-    tolerance: float = 0.05
+    seg: List[float], ref: List[float], tolerance: float = 0.05
 ) -> Tuple[float, float, float]:
 
     n_tokens_seg, n_tokens_ref, n_tokens_hit = 0, 0, 0
     assert len(seg) == len(ref)
-    
+
     for i_utterance in range(len(seg)):
         prediction = seg[i_utterance]
         ground_truth = ref[i_utterance]
 
-        seg_segments = [(a,b) for a,b in itertools.pairwise(prediction)]
-        ref_segments = [(a,b) for a,b in itertools.pairwise([0] + ground_truth)]
+        seg_segments = [(a, b) for a, b in itertools.pairwise(prediction)]
+        ref_segments = [(a, b) for a, b in itertools.pairwise([0] + ground_truth)]
 
         ref_intervals = []
         for word_start, word_end in ref_segments:
-            ref_intervals.append((
-                (max(0, word_start - tolerance), word_start + tolerance),
-                (word_end - tolerance, word_end + tolerance)
-                ))
-            
+            ref_intervals.append(
+                (
+                    (max(0, word_start - tolerance), word_start + tolerance),
+                    (word_end - tolerance, word_end + tolerance),
+                )
+            )
+
         n_tokens_ref += len(ref_intervals)
         n_tokens_seg += len(seg_segments)
 
         for seg_start, seg_end in seg_segments:
-            for i_segment, (ref_start_interval, ref_end_interval) in enumerate(ref_intervals):
+            for i_segment, (ref_start_interval, ref_end_interval) in enumerate(
+                ref_intervals
+            ):
                 ref_start_lower, ref_start_upper = ref_start_interval
                 ref_end_lower, ref_end_upper = ref_end_interval
 
-                if (ref_start_lower <= seg_start <= ref_start_upper and ref_end_lower <= seg_end <= ref_end_upper):
+                if (
+                    ref_start_lower <= seg_start <= ref_start_upper
+                    and ref_end_lower <= seg_end <= ref_end_upper
+                ):
                     n_tokens_hit += 1
                     ref_intervals.pop(i_segment)
                     break
-        
+
     return get_p_r_f1(n_tokens_seg, n_tokens_ref, n_tokens_hit)
